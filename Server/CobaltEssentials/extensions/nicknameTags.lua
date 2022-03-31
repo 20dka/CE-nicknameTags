f60053de6a4f3891b63229a58def3c9ff6cee581
Deer McDurr <doni@doni.hu>
2022-03-31 23:38:51 +0200

    update to 3.x, send nametags on join, add comments

--- Server/CobaltEssentials/extensions/nicknameTags.lua
+++ Server/CobaltEssentials/extensions/nicknameTags.lua
@@ -1,20 +1,32 @@
 local M = {}
 
-local function sendGroupTag(player)
+-- send group info about [player] to [target] ID
+local function sendSingleTag(player, target)
+	if not player or not player.permissions or not player.permissions.group then return end -- make sure we don't cause a Lua error
 	local groupstr = player.permissions.group
-	if groupstr == "default" then groupstr = "" end
-	groupstr = groupstr:gsub("^%l", string.upper)
-	--print("sending nickname group as "..groupstr)
-	local yes = player.name..';'..groupstr
-	TriggerClientEvent(-1, "CEsendPlayerTags", yes)
+	if groupstr == "default" then groupstr = "" end -- ignore default group
+	groupstr = groupstr:gsub("^%l", string.upper) -- make uppercase
+	local concat = player.name..';'..groupstr
+	MP.TriggerClientEvent((target or -1), "CEsendPlayerTags", concat)
 end
 
-local function onInit() end
+-- send all players' group info to [target] ID
+local function sendAllTags(target)
+	for id,p in pairs(players) do
+		if type(id) == "number" then
+			sendSingleTag(p, target)
+		end
+	end
+end
 
-local function onVehicleSpawn(player, vehID,  data) sendGroupTag(player) end
+-- tell everyone about the new spawn's group
+local function onVehicleSpawn(player, ...) sendSingleTag(player) end
 
-M.onInit = onInit
+-- tell the new player about all already joined players' groups
+local function onPlayerJoin(player, ...) sendAllTags(player.playerID) end
+
 M.onVehicleSpawn = onVehicleSpawn
 M.onVehicleEdited = onVehicleSpawn
+M.onPlayerJoin = onPlayerJoin
 
 return M
